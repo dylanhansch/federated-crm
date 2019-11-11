c420e3beb191885ba6aad7cfc1ba2b3a11abc679
Dylan Hansch <dylan@dylanhansch.net>
2019-11-11 10:02:01 -0600

    Separate generic dashboard app layout from core layout
    So login, password recovery, etc. don't need to use the "dashboard layout".

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,48 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<meta name="description" content="">
-		<meta name="theme-color" content="#9a3324">
+@extends('layouts.core')
 
-		<!-- CSRF Token -->
-		<meta name="csrf-token" content="{{ csrf_token() }}">
+@section('app')
+	@include('layouts.navbar')
 
-		<title>@yield('title') - {{ config('app.name') }}</title>
+	<div class="container-fluid">
+		<div class="row">
+			@include('layouts.sidebar')
 
-		<meta property="og:title" content="@yield('title') - {{ config('app.name') }}">
-		<meta property="og:description" content="">
-		<meta property="og:type" content="website">
-		<meta property="og:url" content="{{ request()->fullUrl() }}">
-		<meta property="og:locale" content="en_US">
+			<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+				@yield('content')
 
-		<!-- Scripts -->
-		<script src="{{ asset('assets/js/app.js') }}" defer></script>
-
-		<!-- Styles -->
-		<link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
-
-		<!-- Favicons -->
-		<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicons/favicon-32x32.png') }}">
-		<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicons/favicon-16x16.png') }}">
-	</head>
-	<body>
-		<div id="app">
-			@include('layouts.navbar')
-
-			<div class="container-fluid">
-				<div class="row">
-					@include('layouts.sidebar')
-
-					<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-						@yield('content')
-
-						@include('layouts.footer')
-					</main>
-				</div>
-			</div>
+				@include('layouts.footer')
+			</main>
 		</div>
-	</body>
-</html>
+	</div>
+@endsection
