b23b60097dc7497a1b4081b74feeb2c3b0e23b51
Dylan Hansch <dylan@dylanhansch.net>
2019-11-15 18:54:26 -0600

    Can't generate random int larger than 9 digits

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
 		'email' => $faker->unique()->safeEmail,
 		'first_name' => $faker->firstName,
 		'last_name' => $faker->lastName,
-		'phone_number' => (string) $faker->randomNumber(13),
+		'phone_number' => (string) $faker->randomNumber(9) . (string) $faker->randomNumber(4),
 		'email_verified_at' => now(),
 		'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
 		'remember_token' => Str::random(10),
