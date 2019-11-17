dbacd0f460ff90a3e910fc60e7aabac02b2a6678
Dylan Hansch <dylan@dylanhansch.net>
2019-11-17 12:37:30 -0600

    Remove unused imports

--- src/database/factories/DistrictFactory.php
+++ src/database/factories/DistrictFactory.php
@@ -3,7 +3,6 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\District;
-use App\Model;
 use Faker\Generator as Faker;
 
 $factory->define(District::class, function (Faker $faker) {
