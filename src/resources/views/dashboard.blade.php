7d0d371a44ca4bb533f001c859f6440b3363ce3f
Dylan Hansch <dylan@dylanhansch.net>
2019-11-30 13:01:41 -0600

    Knock out boilerplate dashboard and add non-functioning todo

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -5,146 +5,39 @@
 @section('content')
 	<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 		<h1 class="h2">Dashboard</h1>
-		<div class="btn-toolbar mb-2 mb-md-0">
-			<div class="btn-group mr-2">
-				<button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-				<button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-			</div>
-			<button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-				<span data-feather="calendar"></span>
-				This week
-			</button>
-		</div>
 	</div>
 
-	<canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+	<!-- TODO: Idk what to add here but we need more.. -->
 
-	<h2>Section title</h2>
-	<div class="table-responsive">
-		<table class="table table-striped table-sm">
-			<thead>
-			<tr>
-				<th>#</th>
-				<th>Header</th>
-				<th>Header</th>
-				<th>Header</th>
-				<th>Header</th>
-			</tr>
-			</thead>
-			<tbody>
-			<tr>
-				<td>1,001</td>
-				<td>Lorem</td>
-				<td>ipsum</td>
-				<td>dolor</td>
-				<td>sit</td>
-			</tr>
-			<tr>
-				<td>1,002</td>
-				<td>amet</td>
-				<td>consectetur</td>
-				<td>adipiscing</td>
-				<td>elit</td>
-			</tr>
-			<tr>
-				<td>1,003</td>
-				<td>Integer</td>
-				<td>nec</td>
-				<td>odio</td>
-				<td>Praesent</td>
-			</tr>
-			<tr>
-				<td>1,003</td>
-				<td>libero</td>
-				<td>Sed</td>
-				<td>cursus</td>
-				<td>ante</td>
-			</tr>
-			<tr>
-				<td>1,004</td>
-				<td>dapibus</td>
-				<td>diam</td>
-				<td>Sed</td>
-				<td>nisi</td>
-			</tr>
-			<tr>
-				<td>1,005</td>
-				<td>Nulla</td>
-				<td>quis</td>
-				<td>sem</td>
-				<td>at</td>
-			</tr>
-			<tr>
-				<td>1,006</td>
-				<td>nibh</td>
-				<td>elementum</td>
-				<td>imperdiet</td>
-				<td>Duis</td>
-			</tr>
-			<tr>
-				<td>1,007</td>
-				<td>sagittis</td>
-				<td>ipsum</td>
-				<td>Praesent</td>
-				<td>mauris</td>
-			</tr>
-			<tr>
-				<td>1,008</td>
-				<td>Fusce</td>
-				<td>nec</td>
-				<td>tellus</td>
-				<td>sed</td>
-			</tr>
-			<tr>
-				<td>1,009</td>
-				<td>augue</td>
-				<td>semper</td>
-				<td>porta</td>
-				<td>Mauris</td>
-			</tr>
-			<tr>
-				<td>1,010</td>
-				<td>massa</td>
-				<td>Vestibulum</td>
-				<td>lacinia</td>
-				<td>arcu</td>
-			</tr>
-			<tr>
-				<td>1,011</td>
-				<td>eget</td>
-				<td>nulla</td>
-				<td>Class</td>
-				<td>aptent</td>
-			</tr>
-			<tr>
-				<td>1,012</td>
-				<td>taciti</td>
-				<td>sociosqu</td>
-				<td>ad</td>
-				<td>litora</td>
-			</tr>
-			<tr>
-				<td>1,013</td>
-				<td>torquent</td>
-				<td>per</td>
-				<td>conubia</td>
-				<td>nostra</td>
-			</tr>
-			<tr>
-				<td>1,014</td>
-				<td>per</td>
-				<td>inceptos</td>
-				<td>himenaeos</td>
-				<td>Curabitur</td>
-			</tr>
-			<tr>
-				<td>1,015</td>
-				<td>sodales</td>
-				<td>ligula</td>
-				<td>in</td>
-				<td>libero</td>
-			</tr>
-			</tbody>
-		</table>
+	<div class="row">
+		<div class="col-md-4">
+			<h2>Today's To-Do</h2>
+			<ul class="list-group">
+				<li class="list-group-item">
+					<div class="form-check">
+						<input class="form-check-input" type="checkbox" value="" id="todoCheck1">
+						<label class="form-check-label" for="todoCheck1">
+							Totally
+						</label>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="form-check">
+						<input class="form-check-input" type="checkbox" value="" id="todoCheck2">
+						<label class="form-check-label" for="todoCheck2">
+							Non-functioning
+						</label>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="form-check">
+						<input class="form-check-input" type="checkbox" value="" id="todoCheck3">
+						<label class="form-check-label" for="todoCheck3">
+							To-do list
+						</label>
+					</div>
+				</li>
+			</ul>
+		</div>
 	</div>
 @endsection
