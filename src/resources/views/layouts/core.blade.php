3818d95a68421777725987a94ec68b47712823ff
Dylan Hansch <dylan@dylanhansch.net>
2019-12-02 12:01:44 -0600

    Update theme color to match federated site

--- src/resources/views/layouts/core.blade.php
+++ src/resources/views/layouts/core.blade.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<meta name="description" content="">
-		<meta name="theme-color" content="#9B2F22">
+		<meta name="theme-color" content="#9a3324">
 
 		<!-- CSRF Token -->
 		<meta name="csrf-token" content="{{ csrf_token() }}">
