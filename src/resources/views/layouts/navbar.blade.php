13b3807108b3667a6ace8bbe30932bbc8426c8fe
Dylan Hansch <dylan@dylanhansch.net>
2019-12-02 12:17:04 -0600

    Add federated logo to brand area in navbar

--- src/resources/views/layouts/navbar.blade.php
+++ src/resources/views/layouts/navbar.blade.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-	<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('dashboard') }}">{{ config('app.name') }}</a>
+	<a class="navbar-brand col-sm-3 col-md-2 mr-0 text-center" href="{{ route('dashboard') }}">
+		<img src="{{ asset('assets/img/logo-small.png') }}" class="img-fluid brand-img" alt="{{ config('app.name') }} Logo">
+	</a>
 	<ul class="navbar-nav px-3">
 		<li class="nav-item text-nowrap">
 			<a class="nav-link" href="{{ route('logout') }}">Sign out</a>
